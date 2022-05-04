c2940d769e24ed90c5feafa2ad822ddcc9ccdcae
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-03 22:58:50 -0500

    changed to show message in the HTML rather than in the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ const inputHigh = prompt('Please provide a number');
 // Convert the input to a number
 const highNumber = parseInt(inputHigh);
 
+// Select the <main> HTML element
+const main = document.querySelector('main');
+
 // Use Math.random() and the user's number to generate a random number
 const randomNumber = Math.floor( Math.random() * highNumber ) + 1;
 
 // Create a message displaying the random number
-console.log(`${randomNumber} is a random number between 1 and ${highNumber}.`);
+main.innerHTML = `
+    <h2>${randomNumber} is a random number between 1 and ${highNumber}.
+
+`;
